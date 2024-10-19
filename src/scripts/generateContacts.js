a8e9dce088ef92d6e4fc012fb4ab30cf2b2d2495
Yana17-ratsa <ratsayana13@gmail.com>
2024-10-19 15:40:53 +0300

    tasks done

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,11 @@
+import { createFakeContact } from "../utils/createFakeContact.js";
+import {readContacts} from "../utils/readContacts.js";
+import {writeContacts} from "../utils/writeContacts.js";
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    const newContatcs = Array(number).fill(0).map(createFakeContact);
+    const result = [...contacts, ...newContatcs];
+    await writeContacts(result);
+};
+
+generateContacts(5);
